66cefa6de1072c36582f2e7bae0ca4e451e983a1
Manmeet Singh <smanmeet090@gmail.com>
2020-11-24 15:54:03 +0530

    Update apk_install_list.py

--- apk_install_list.py
+++ apk_install_list.py
@@ -6,4 +6,20 @@ for ele in list:
     """
  in square brackets like ["bromite","brave"] and then run this python script and also paste apk in apk folder with same name
 
+ eg: 
+
+Input:
+
+	  ["whatsapp","facebook","google","chrome","orbot","b612"]
+
+ output :
+
+		adb install "apk/whatsapp.apk" > CON
+		adb install "apk/facebook.apk" > CON
+		adb install "apk/google.apk" > CON
+		adb install "apk/chrome.apk" > CON
+		adb install "apk/orbot.apk" > CON
+		adb install "apk/b612.apk" > CON
+
+
 """
