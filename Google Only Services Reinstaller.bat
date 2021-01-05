7e823c2380e17d964874583b9f45020c2b645f27
Manmeet Singh <74925650+meetu2001@users.noreply.github.com>
2021-01-05 15:09:24 +0000

    Update Google Only Services Reinstaller.bat

--- Google Only Services Reinstaller.bat
+++ Google Only Services Reinstaller.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 ECHO ####### Let Me Install Google Services For You #######
 adb devices
-adb install "apk/com.google.android.gms.apk" > CON
-adb install "apk/com.android.vending.apk" > CON
-adb install "apk/com.google.android.gsf.apk" > CON
+adb install-existing "com.google.android.gms" > CON
+adb install-existing "com.android.vending" > CON
+adb install-existing "com.google.android.gsf" > CON
 adb kill-server
 ECHO ###### DONE ########
 ECHO Now, you will be able to login to google account and use google. For Full google install refer to other script file
