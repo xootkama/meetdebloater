7be6174e1d227d306e0c50073c816bf146582b0f
Manmeet Singh <74925650+meetu2001@users.noreply.github.com>
2021-01-05 15:12:01 +0000

    Update Google Full  Reinstaller.bat

--- Google Full  Reinstaller.bat
+++ Google Full  Reinstaller.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 ECHO ####### Let Me Install Full Google Services For You #######
 adb devices
-adb install "apk/com.google.android.gm.apk" > CON
-adb install "apk/com.android.chrome.apk" > CON
-adb install "apk/com.google.android.apps.maps.apk" > CON
-adb install "apk/com.google.android.gms.apk" > CON
-adb install "apk/com.google.android.googlequicksearchbox.apk" > CON
-adb install "apk/com.google.android.youtube.apk" > CON
-adb install "apk/com.android.vending.apk" > CON
-adb install "apk/com.google.android.gsf.apk" > CON
+adb install-existing "com.google.android.gm" > CON
+adb install-existing "com.android.chrome" > CON
+adb install-existing "com.google.android.apps.maps" > CON
+adb install-existing "com.google.android.gms" > CON
+adb install-existing "com.google.android.googlequicksearchbox" > CON
+adb install-existing "com.google.android.youtube" > CON
+adb install-existing "com.android.vending" > CON
+adb install-existing "com.google.android.gsf" > CON
 adb kill-server
 ECHO ###### DONE ########
 PAUSE
