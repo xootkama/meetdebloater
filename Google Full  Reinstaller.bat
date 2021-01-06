f027a94c299db512791731cdc7e8694cef15f245
Manmeet Singh <smanmeet090@gmail.com>
2021-01-06 06:12:01 +0530

    bug fixed

--- Google Full  Reinstaller.bat
+++ Google Full  Reinstaller.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 ECHO ####### Let Me Install Full Google Services For You #######
 adb devices
-adb install-existing "com.google.android.gm" > CON
-adb install-existing "com.android.chrome" > CON
-adb install-existing "com.google.android.apps.maps" > CON
-adb install-existing "com.google.android.gms" > CON
-adb install-existing "com.google.android.googlequicksearchbox" > CON
-adb install-existing "com.google.android.youtube" > CON
-adb install-existing "com.android.vending" > CON
-adb install-existing "com.google.android.gsf" > CON
+adb shell "install-existing com.google.android.gm" > CON
+adb shell "install-existing com.android.chrome" > CON
+adb shell "install-existing com.google.android.apps.maps" > CON
+adb shell "install-existing com.google.android.gms" > CON
+adb shell "install-existing com.google.android.googlequicksearchbox" > CON
+adb shell "install-existing com.google.android.youtube" > CON
+adb shell "install-existing com.android.vending" > CON
+adb shell "install-existing com.google.android.gsf" > CON
 adb kill-server
 ECHO ###### DONE ########
 PAUSE
--- Google Only Services Reinstaller.bat
+++ Google Only Services Reinstaller.bat
@@ -1,9 +1,9 @@
 @ECHO OFF
 ECHO ####### Let Me Install Google Services For You #######
 adb devices
-adb install-existing "com.google.android.gms" > CON
-adb install-existing "com.android.vending" > CON
-adb install-existing "com.google.android.gsf" > CON
+adb shell "pm install-existing com.google.android.gms" > CON
+adb shell "pm install-existing com.android.vending" > CON
+adb shell "pm install-existing com.google.android.gsf" > CON
 adb kill-server
 ECHO ###### DONE ########
 ECHO Now, you will be able to login to google account and use google. For Full google install refer to other script file
